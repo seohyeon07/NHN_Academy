69858865ff6784af49ee284bc5cb2a359d117c82
seohyeon07 <77095429+seohyeon07@users.noreply.github.com>
2022-11-23 09:25:20 +0900

    Delete exchangeProject directory

--- exchangeProject/src/main/java/com/nhnacademy/gwjs/exception/CreateMoneyWithNegativeNumbers.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.gwjs.exception;
-
-public class CreateMoneyWithNegativeNumbers extends RuntimeException {
-    public CreateMoneyWithNegativeNumbers(double amount) {
-        super("Amount must be positive numbers. Current amount: " + amount);
-    }
-}
--- exchangeProject/src/main/java/com/nhnacademy/gwjs/exception/SameCurrencyException.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.gwjs.exception;
-
-import com.nhnacademy.gwjs.entity.Currency;
-
-public class SameCurrencyException extends RuntimeException {
-    public SameCurrencyException(Currency currency) {
-        super("Same currency. Cannot be exchanged : " + currency);
-    }
-}
